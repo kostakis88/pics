5b72997d8432b7194eae1f93c7e85b1849c6e8e9
kostakis88 <kostasdallas2010@hotmail.com>
2019-03-02 15:39:15 +0200

    semantic ui, axios and serch bar functionality

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query : term
+            },
+            headers: {
+                Authorization: 'Client-ID 2988d00045927d2dddae5fac472dcf7aa9884fe21d8310e9ecb07ea12251bf6a'
+            }
+        });
+
+        console.log(response.data.results);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+}
+
+export default App;
